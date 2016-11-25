c024ac0c719fadd4b20b9ee45d4cc7d49aae1dd3
Kevin <kmasbergen@hotmail.com>
2016-11-25 09:36:41 +0100

    string tutorial

--- src/tutorial/Test.java
+++ src/tutorial/Test.java
@@ -2,6 +2,7 @@ package tutorial;
 
 public class Test {
 	public static void main(String args[]) {
+		   
 	      int [] numbers = {10, 20, 30, 40, 50};
 
 	      for(int x : numbers ) {
@@ -15,5 +16,18 @@ public class Test {
 	         System.out.print( name );
 	         System.out.print(",");
 	      }
+	     
+	      Integer x = 5; // boxes int to an Integer object
+	      x =  x + 10;   // unboxes the Integer to a int
+	      System.out.println(x); 
+	      
+	      char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.' };
+	      String helloString = new String(helloArray);  
+	      System.out.println( helloString );
+	      
+	      String palindrome = "Dot saw I was Tod";
+	      int len = palindrome.length();
+	      System.out.println( "String Length is : " + len );
 	   }
+		
 }
